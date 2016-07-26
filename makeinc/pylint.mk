41e98944576413f8d3db6b9929b9a659db28076b
Martin Ejdestig <marejde@gmail.com>
2016-07-26 14:58:43 +0200

    Fix pylint make target on Debian and Debian derivatives

--- makeinc/pylint.mk
+++ makeinc/pylint.mk
@@ -1,10 +1,13 @@
 PYLINT_RUN := $(PYTHON_SRC:%=pylint-run-%)
 PYLINT_FLAGS ?= --reports=n --msg-template="{path}:{line}: {msg} [{symbol}]"
 
+# Some distributions ship Pylint for Python 3 as pylint3.
+PYLINT_EXE := $(if $(shell pylint3 --version 2> /dev/null),pylint3,pylint)
+
 .PHONY: pylint $(PYLINT_RUN)
 
 pylint: $(PYLINT_RUN)
 
 $(PYLINT_RUN): pylint-run-% : %
 	@echo pylint $<
-	@pylint $(PYLINT_FLAGS) $< 2>&1 | grep -v "^\*\+ Module " || true
+	@$(PYLINT_EXE) $(PYLINT_FLAGS) $< 2>&1 | grep -v "^\*\+ Module " || true
