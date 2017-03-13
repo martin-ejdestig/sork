e8b873bbc77606ec29792666b323e88d50d52a11
Martin Ejdestig <marejde@gmail.com>
2017-03-13 17:22:41 +0100

    Disable showing score and storing of pylint data for later comparisons
    Later versions of pylint (>= what is in Arch as of today at least)
    apparently started to report score even if reports are disabled.
    
    Also disable storing of persistent data because we are not interested in
    comparing against older runs. We are only interested in being told where
    the errors/warnings are.

--- makeinc/pylint.mk
+++ makeinc/pylint.mk
@@ -1,5 +1,5 @@
 PYLINT_RUN := $(PYTHON_SRC:%=pylint-run-%)
-PYLINT_FLAGS ?= --reports=n --msg-template="{path}:{line}: {msg} [{symbol}]"
+PYLINT_FLAGS ?= --persistent=n --reports=n --score=n --msg-template="{path}:{line}: {msg} [{symbol}]"
 
 # Some distributions ship Pylint for Python 3 as pylint3.
 PYLINT_EXE := $(if $(shell pylint3 --version 2> /dev/null),pylint3,pylint)
