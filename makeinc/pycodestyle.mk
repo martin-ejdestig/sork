a799dee13f74e684aa3c092ac2a7b0dc6f1e5775
Martin Ejdestig <marejde@gmail.com>
2017-07-20 13:58:05 +0200

    Fix check target on Fedora
    Fedora appends a -3 suffix to its Python 3 tools. Never seen pycodestyle3,
    but check for it as well before checking for pycodestyle or pep8.
    
    For some reason pycodestyle-3 in Fedora still uses the old [pep8] section.
    Add it back but keep the new one (since pycodestyle on Arch emits warnings
    if only [pep8] is found... *sigh*).
    
    Should work on Arch, Debian and Fedora now.

--- makeinc/pycodestyle.mk
+++ makeinc/pycodestyle.mk
@@ -2,7 +2,9 @@ PYCODESTYLE_RUN := $(PYTHON_SRC:%=pycodestyle-run-%)
 
 # The pep8 tool has been renamed to pycodestyle with its 2.0.0 release.
 # Fallback to pep8 if it does not exist until distributions have caught up.
-PYCODESTYLE_EXE := $(if $(shell pycodestyle --version 2> /dev/null),pycodestyle,pep8)
+# Some distributions ship pycodestyle for Python 3 as pycodestyle3 or pycodestyle-3.
+PYCODESTYLE_EXE := $(firstword $(foreach c,pycodestyle3 pycodestyle-3 pycodestyle pep8,\
+                                 $(if $(shell $c --version 2> /dev/null),$c)))
 
 .PHONY: pycodestyle $(PYCODESTYLE_RUN)
 
--- makeinc/pylint.mk
+++ makeinc/pylint.mk
@@ -1,8 +1,9 @@
 PYLINT_RUN := $(PYTHON_SRC:%=pylint-run-%)
 PYLINT_FLAGS ?= --persistent=n --reports=n --score=n --msg-template="{path}:{line}: {msg} [{symbol}]"
 
-# Some distributions ship Pylint for Python 3 as pylint3.
-PYLINT_EXE := $(if $(shell pylint3 --version 2> /dev/null),pylint3,pylint)
+# Some distributions ship Pylint for Python 3 as pylint3 or pylint-3.
+PYLINT_EXE := $(firstword $(foreach c,pylint3 pylint-3 pylint,\
+                            $(if $(shell $c --version 2> /dev/null),$c)))
 
 .PHONY: pylint $(PYLINT_RUN)
 
