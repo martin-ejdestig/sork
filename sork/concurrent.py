martin-ejdestig/sork

commit 2b138e4d5bcbd95498cda7bda1cd70300a4fcabb

Move use of ProgressPrinter from concurrent to commands
Moving ProgressPrinter to commands will make it easier to e.g. add a
verbose flag and to output name of file that is being processed. This is
not generic enough to have in concurrent.

A bit of repetition with try, catch, abort(), raise etc but will do
something about that at a later point if it is repeated more in e.g. other
commands.

Note that ProgressPrinter is now used from different threads. OK since it
was made thread safe in previous commit.

sork/concurrent.py
@@ -17,24 +17,17 @@
 
 import concurrent.futures
 
-from . import progress_printer
 
-
-def for_each_with_progress_printer(info_string, func, values, num_threads=None):
+def for_each(func, values, num_threads=None):
     aborted = False
-    printer = progress_printer.ProgressPrinter()
 
     def wrapped_func(value):
         return None if aborted else func(value)
 
-    printer.start(info_string, len(values))
-
     with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
         try:
             futures = [executor.submit(wrapped_func, value) for value in values]
-            for future in concurrent.futures.as_completed(futures):
-                printer.result(future.result())
+            concurrent.futures.wait(futures)
         except BaseException:
             aborted = True
-            printer.abort()
             raise

sork/commands/analyze.py
@@ -20,6 +20,7 @@ import subprocess
 
 from .. import command
 from .. import concurrent
+from .. import progress_printer
 from .. import source
 
 
@@ -43,10 +44,17 @@ def _analyze_source_file(source_file):
 
 
 def _analyze_source_files(args, source_files):
-    concurrent.for_each_with_progress_printer('Analyzing source',
-                                              _analyze_source_file,
-                                              source_files,
-                                              num_threads=args.jobs)
+    printer = progress_printer.ProgressPrinter()
+    printer.start('Analyzing source', len(source_files))
+
+    def analyze(source_file):
+        printer.result(_analyze_source_file(source_file))
+
+    try:
+        concurrent.for_each(analyze, source_files, num_threads=args.jobs)
+    except BaseException:
+        printer.abort()
+        raise
 
 
 def _source_files_with_compile_command(args, environment):

sork/commands/check.py
@@ -21,6 +21,7 @@ from .. import check
 from .. import checks
 from .. import command
 from .. import concurrent
+from .. import progress_printer
 from .. import source
 
 
@@ -56,16 +57,20 @@ def _get_enabled_checks(args, environment):
 
 def _run_checks(args, environment):
     enabled_checks = _get_enabled_checks(args, environment)
+    source_files = source.find_source_files(environment, args.source_paths)
+
+    printer = progress_printer.ProgressPrinter()
+    printer.start('Checking source', len(source_files))
 
     def check_source_file(source_file):
         outputs = (c.check(source_file) for c in enabled_checks)
-        return '\n'.join(o for o in outputs if o)
+        printer.result('\n'.join(o for o in outputs if o))
 
-    concurrent.for_each_with_progress_printer('Checking source',
-                                              check_source_file,
-                                              source.find_source_files(environment,
-                                                                       args.source_paths),
-                                              num_threads=args.jobs)
+    try:
+        concurrent.for_each(check_source_file, source_files, num_threads=args.jobs)
+    except BaseException:
+        printer.abort()
+        raise
 
 
 class CheckCommand(command.Command):

commit 85f9dbb5408ef7875bd04f631e34649dd1db66b9

Fix concurrent.for_each() so all exceptions are reraised
Discovered when writing tests for concurrent module (see following commit).

Regressed in 2b138e4d5bcbd95498cda7bda1cd70300a4fcabb (Future.result() used
before that rethrows if there was an exception).

Do not fully understand why Ctrl-C still worked correctly without this but
... whatever. It should be more correct now.

sork/concurrent.py
@@ -33,7 +33,9 @@ def for_each(func: Callable[[T], None], values: Sequence[T], num_threads: Option
     with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
         try:
             futures = [executor.submit(wrapped_func, value) for value in values]
-            concurrent.futures.wait(futures)
+            for future in concurrent.futures.as_completed(futures):
+                if future.exception():
+                    raise future.exception()
         except BaseException:
             aborted = True
             raise