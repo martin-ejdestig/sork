fd20966f87f19a13fb93716847063b4b56431870
Martin Ejdestig <marejde@gmail.com>
2017-09-10 16:38:18 +0200

    Decrease time it takes to run make check
    "make -j8 check" before change takes around 13.9s. After this change it
    takes around 4.9s.
    
    Discovered that pylint has a -j/--jobs flag. Use it instead of spawning
    separate pylint processes (seems to be a large overhead to do this) for
    each source file. Extract -j flag from MAKEFLAGS so the number of jobs
    matches what user pass to make.
    
    Running pycodestyle separately on each source file takes around 4.6s with
    "make -j8". Running "pycodestyle sork" takes 0.8s. Could not find a
    -j/--jobs flag for pycodestyle.

--- makeinc/pycodestyle.mk
+++ makeinc/pycodestyle.mk
@@ -1,14 +1,10 @@
-PYCODESTYLE_RUN := $(PYTHON_SRC:%=pycodestyle-run-%)
-
 # The pep8 tool has been renamed to pycodestyle with its 2.0.0 release.
 # Fallback to pep8 if it does not exist until distributions have caught up.
 # Some distributions ship pycodestyle for Python 3 as pycodestyle3 or pycodestyle-3.
 PYCODESTYLE_EXE := $(firstword $(foreach c,pycodestyle3 pycodestyle-3 pycodestyle pep8,\
                                  $(if $(shell $c --version 2> /dev/null),$c)))
 
-.PHONY: pycodestyle $(PYCODESTYLE_RUN)
+.PHONY: pycodestyle
 
-pycodestyle: $(PYCODESTYLE_RUN)
-
-$(PYCODESTYLE_RUN): pycodestyle-run-% : %
-	$(PYCODESTYLE_EXE) $<
+pycodestyle:
+	$(PYCODESTYLE_EXE) sork
--- makeinc/pylint.mk
+++ makeinc/pylint.mk
@@ -1,14 +1,12 @@
-PYLINT_RUN := $(PYTHON_SRC:%=pylint-run-%)
-PYLINT_FLAGS ?= --persistent=n --reports=n --score=n --msg-template="{path}:{line}: {msg} [{symbol}]"
+PYLINT_JOBS_FLAG = $(filter -j%,$(MAKEFLAGS))
+PYLINT_FLAGS ?= $(PYLINT_JOBS_FLAG) --persistent=n --reports=n --score=n --msg-template="{path}:{line}: {msg} [{symbol}]"
 
 # Some distributions ship Pylint for Python 3 as pylint3 or pylint-3.
 PYLINT_EXE := $(firstword $(foreach c,pylint3 pylint-3 pylint,\
                             $(if $(shell $c --version 2> /dev/null),$c)))
 
-.PHONY: pylint $(PYLINT_RUN)
+.PHONY: pylint
 
-pylint: $(PYLINT_RUN)
-
-$(PYLINT_RUN): pylint-run-% : %
-	@echo pylint $<
-	@$(PYLINT_EXE) $(PYLINT_FLAGS) $< 2>&1 | grep -v "^\*\+ Module " || true
+pylint:
+	@echo pylint sork
+	@$(PYLINT_EXE) $(PYLINT_FLAGS) sork
