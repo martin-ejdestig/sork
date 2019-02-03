2a346aed62be163273befb870afe4d87ce9c6c39
Martin Ejdestig <marejde@gmail.com>
2019-02-03 09:39:10 +0100

    Update year in copyright strings

--- sork/tests/test_paths.py
+++ sork/tests/test_paths.py
@@ -1,6 +1,6 @@
 # This file is part of Sork.
 #
-# Copyright (C) 2017-2018 Martin Ejdestig <marejde@gmail.com>
+# Copyright (C) 2017-2019 Martin Ejdestig <marejde@gmail.com>
 #
 # Sork is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
