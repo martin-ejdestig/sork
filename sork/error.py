martin-ejdestig/sork

commit 4cfdc6330a4b1faaea75f8ce2e1dc8305b05e129

Add SPDX-License-Identifier to all source files
Again, sad about -or-later instead of + ... but do not want to use
deprecated format. :(

sork/error.py
@@ -14,7 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
-
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 class Error(Exception):
     pass

sork.py
@@ -16,6 +16,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from sork.main import main
 

sork/arguments.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import argparse
 import os

sork/checks/check.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from typing import Callable, Optional
 

sork/checks/clang_format.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import difflib
 import subprocess

sork/checks/clang_tidy.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
 import re

sork/checks/create.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import re
 

sork/checks/include_guard.py
@@ -14,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Sork. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
 import re

commit 7bbefd18773837f447a5cbe4afc91ccacb7224de

Add missing newline to fix accidentally broken 'make check'
Broke in 4cfdc6330a4b1faaea75f8ce2e1dc8305b05e129 .

sork/error.py
@@ -17,5 +17,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+
 class Error(Exception):
     pass